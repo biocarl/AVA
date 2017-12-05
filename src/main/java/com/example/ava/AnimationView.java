3762451f7a5bde4c1ff25f5bd285216ca48736c0
Carl Hauck <cahauck@students.uni-mainz.de>
2017-12-05 14:23:03 -0200

    init repo

---
+++ src/main/java/com/example/ava/AnimationView.java
@@ -0,0 +1,14 @@
+package com.example.ava;
+
+import android.content.Context;
+import android.widget.ImageSwitcher;
+
+/**
+ * Created by carlh on 05.12.2017.
+ */
+
+public class AnimationView extends ImageSwitcher {
+    public AnimationView(Context context) {
+        super(context);
+    }
+}
